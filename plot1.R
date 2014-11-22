30ae699f470224b4760823703fcded3a1b31df98
gfilippini <g.filippini@hotmail.com>
2014-11-22 14:04:27 -0200

    first plot for Project 2 - Exploratory Data Analysis - Coursera

---
+++ plot1.R
@@ -0,0 +1,26 @@
+
+
+## Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? 
+## Using the base plotting system, make a plot showing the total PM2.5 emission from 
+## all sources for each of the years 1999, 2002, 2005, and 2008
+
+## creating figures directory
+if(!file.exists("./ExData_Ploting2/figures")){dir.create("./ExData_Ploting2/figures")}
+
+## reading data
+## This first line will likely take a few seconds. Be patient!
+if (!"NEI" %in% ls()) {
+  NEI <- readRDS("./data/summarySCC_PM25.rds")
+}
+if (!"SCC" %in% ls()) {
+  SCC <- readRDS("./data/Source_Classification_Code.rds")
+}
+
+totalemissions<-aggregate(NEI$Emissions,list(NEI$year),FUN="sum")
+
+options (scipen=999)
+
+png("./ExData_Ploting2/figures/plot1.png",width=480, height=480)
+plot(totalemissions,type='l',xlab="Year",ylab=expression('Total PM'[2.5]*" Emission"),
+     main=expression('PM'[2.5]*" emssion over the years 1999 - 2008"))
+dev.off()
